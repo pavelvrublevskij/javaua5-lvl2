f68a230036a14269dedfe490cad8758bbe390f1c
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-10-16 20:17:23 +0300

    [git] Student.java dto/model class renamed into StudentDto.java

--- P1Git/src/lt/codeacademy/lv2/git/StudentDto.java
+++ P1Git/src/lt/codeacademy/lv2/git/StudentDto.java
@@ -1,10 +1,10 @@
 package lt.codeacademy.lv2.git;
 
-public class Student {
+public class StudentDto {
 
 	private final String name;
 
-	public Student(String name) {
+	public StudentDto(String name) {
 		this.name = name;
 	}
 
