b8ec6ce3ce8a915b316cd7b368564c0ab746e569
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-10-16 21:32:28 +0300

    primitive types example

--- P1Git/src/lt/codeacademy/lv2/git/P1GitApp.java
+++ P1Git/src/lt/codeacademy/lv2/git/P1GitApp.java
@@ -3,6 +3,17 @@ package lt.codeacademy.lv2.git;
 public class P1GitApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
+		int a = 5;
+		boolean b = true; // true or false
+		float c = 5.5f;
+		double d = 5.5;
+
+		char e = 'a'; // ASCII table
+		// or
+		char eDigit = 56; // 56 = 8
+
+		byte f = 127; // from -128 to 127
+		long g = 1234567890; //from -2^63 to 2^63-1
+		short h = 32767; // from -32768 to 32767
 	}
 }
