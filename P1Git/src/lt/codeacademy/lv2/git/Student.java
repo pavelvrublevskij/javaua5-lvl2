c091d25a59cd868344d6ce34fde1a293f90c4273
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-10-16 19:48:43 +0300

    [git] add new Student dto/model class

---
+++ P1Git/src/lt/codeacademy/lv2/git/Student.java
@@ -0,0 +1,16 @@
+package lt.codeacademy.lv2.git;
+
+public class Student {
+
+	private final String name;
+
+	public Student(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+}
