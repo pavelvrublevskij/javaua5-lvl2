d84ae38fbd8c1626d1bc4b90179ab4fd2b565ef8
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2023-10-24 19:47:32 +0300

    create new additional empty project as module

---
+++ P7JacksonJson/src/main/java/lt/codeacademy/javaua5/lvl2/json/App.java
@@ -0,0 +1,11 @@
+package lt.codeacademy.javaua5.lvl2.json;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
