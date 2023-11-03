293ded9f78c84f0310c799d292c134012261f56a
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2023-11-03 18:25:03 +0200

    java record init module with standard immutable class example

---
+++ P13RecordAndLombok/src/main/java/lt/codeacademy/javaua5/lvl2/recordandlambok/App.java
@@ -0,0 +1,11 @@
+package lt.codeacademy.javaua5.lvl2.recordandlambok;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ P13RecordAndLombok/src/main/java/lt/codeacademy/javaua5/lvl2/recordandlambok/entity/AnimalEntity.java
@@ -0,0 +1,35 @@
+package lt.codeacademy.javaua5.lvl2.recordandlambok.entity;
+
+// immutable class
+// what we can do with this class:
+// * do sort
+// * create object
+// * assign via constructor data
+// * get data using get...() methods
+// * inherit class
+// * implement interfaces
+// * do overrides, do overloads
+public class AnimalEntity {
+
+	private final int age;
+	private final String name;
+	private final String number;
+
+	public AnimalEntity(int age, String name, String number) {
+		this.age = age;
+		this.name = name;
+		this.number = number;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+}
