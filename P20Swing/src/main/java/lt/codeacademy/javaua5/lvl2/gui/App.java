603a804a56bb52928fe9ad377b0f697dfa5392b1
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2023-11-13 20:44:22 +0200

    swing - refactor using composition

--- P20Swing/src/main/java/lt/codeacademy/javaua5/lvl2/gui/App.java
+++ P20Swing/src/main/java/lt/codeacademy/javaua5/lvl2/gui/App.java
@@ -1,54 +1,11 @@
 package lt.codeacademy.javaua5.lvl2.gui;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 /**
  * Hello world!
  */
 public class App {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("My First GUI");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300, 600);
-
-		DefaultListModel<String> model = new DefaultListModel<>();
-		JList<String> sarasas = new JList<>(model);
-
-		JLabel labelVardas = new JLabel("Vardas");
-		JTextField vardasTextField = new JTextField(20);
-		JLabel labelPavarde = new JLabel("Pavarde");
-		JTextField pavardeTextField = new JTextField(20);
-		JButton itrauktiMygtukas = new JButton("Itraukti i sarasa");
-		JButton isvalymoMygtukas = new JButton("Isvalyti sarasa");
-
-		itrauktiMygtukas.addActionListener(e -> {
-			String ivestasVardas = vardasTextField.getText();
-			String ivestaPavarde = pavardeTextField.getText();
-			model.addElement(ivestasVardas + " " + ivestaPavarde);
-			vardasTextField.setText(null);
-			pavardeTextField.setText(null);
-		});
-
-		isvalymoMygtukas.addActionListener(e -> model.clear());
-
-		JPanel panelVirsus = new JPanel();
-		panelVirsus.setPreferredSize(new Dimension(300, 100));
-		JPanel panelCentras = new JPanel();
-		panelVirsus.add(labelVardas);
-		panelVirsus.add(vardasTextField);
-		panelVirsus.add(labelPavarde);
-		panelVirsus.add(pavardeTextField);
-		panelVirsus.add(itrauktiMygtukas);
-		panelVirsus.add(isvalymoMygtukas);
-		panelCentras.add(sarasas);
-
-		frame.getContentPane().add(BorderLayout.NORTH, panelVirsus);
-		frame.getContentPane().add(BorderLayout.CENTER, panelCentras);
-
-		frame.setVisible(true);
+		new MainApplicationFrame();
 	}
 }
---
+++ P20Swing/src/main/java/lt/codeacademy/javaua5/lvl2/gui/MainApplicationFrame.java
@@ -0,0 +1,61 @@
+package lt.codeacademy.javaua5.lvl2.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainApplicationFrame extends JFrame {
+
+	private JButton itrauktiMygtukas;
+	private JButton isvalymoMygtukas;
+	private JTextField vardasTextField;
+	private JTextField pavardeTextField;
+	private DefaultListModel<String> model = new DefaultListModel<>();
+
+	public MainApplicationFrame() {
+		super("My First GUI");
+		createComponents();
+		addActionListeners();
+	}
+
+	private void addActionListeners() {
+		itrauktiMygtukas.addActionListener(e -> {
+			String ivestasVardas = vardasTextField.getText();
+			String ivestaPavarde = pavardeTextField.getText();
+			model.addElement(ivestasVardas + " " + ivestaPavarde);
+			vardasTextField.setText(null);
+			pavardeTextField.setText(null);
+		});
+
+		isvalymoMygtukas.addActionListener(e -> model.clear());
+	}
+
+	private void createComponents() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(300, 600);
+
+		JList<String> sarasas = new JList<>(model);
+
+		JLabel labelVardas = new JLabel("Vardas");
+		vardasTextField = new JTextField(20);
+		JLabel labelPavarde = new JLabel("Pavarde");
+		pavardeTextField = new JTextField(20);
+		itrauktiMygtukas = new JButton("Itraukti i sarasa");
+		isvalymoMygtukas = new JButton("Isvalyti sarasa");
+
+		JPanel panelVirsus = new JPanel();
+		panelVirsus.setPreferredSize(new Dimension(300, 100));
+		JPanel panelCentras = new JPanel();
+		panelVirsus.add(labelVardas);
+		panelVirsus.add(vardasTextField);
+		panelVirsus.add(labelPavarde);
+		panelVirsus.add(pavardeTextField);
+		panelVirsus.add(itrauktiMygtukas);
+		panelVirsus.add(isvalymoMygtukas);
+		panelCentras.add(sarasas);
+
+		this.getContentPane().add(BorderLayout.NORTH, panelVirsus);
+		this.getContentPane().add(BorderLayout.CENTER, panelCentras);
+
+		this.setVisible(true);
+	}
+}
